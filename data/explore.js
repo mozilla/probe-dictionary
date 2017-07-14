d1877ef4f53eca62bccf14c7807f8f3afa903cb4
Georg Fritzsche <georg.fritzsche@gmail.com>
2017-07-14 04:00:21 +0200

    Fix instrumentation consistency

--- data/explore.js
+++ data/explore.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
   ];
 
   Promise.all(loads).then(values => {
+    mark("all json loaded");
     [gGeneralData, gRevisionsData, gProbeData] = values;
 
     extractChannelInfo();
